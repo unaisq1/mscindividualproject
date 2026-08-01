1362af0028f1c2b87f4a8fd4ec1f3d90e27048a6
Unais Qureshi <unaisq1@gmail.com>
2026-08-01 01:08:10 +0100

    Voting System class completed

--- Model Deliverable/JavaImplementation/JavaImplementation/src/com/safetyandsecurityinterplay/component/FusionProcessor.java
+++ Model Deliverable/JavaImplementation/JavaImplementation/src/com/safetyandsecurityinterplay/component/FusionProcessor.java
@@ -2,4 +2,11 @@ package com.safetyandsecurityinterplay.component;
 
 public class FusionProcessor {
     
+    //Attributes/Variables
+
+    //Constructor
+    
+    //Setters
+
+    //Getters
 }
--- Model Deliverable/JavaImplementation/JavaImplementation/src/com/safetyandsecurityinterplay/component/VotingSystem.java
+++ Model Deliverable/JavaImplementation/JavaImplementation/src/com/safetyandsecurityinterplay/component/VotingSystem.java
@@ -1,5 +1,38 @@
 package com.safetyandsecurityinterplay.component;
 
 public class VotingSystem {
-    
+
+    //Attributes/Variables
+    private int minimumVotes;
+    private int totalSources;
+
+    //Constructor
+    public VotingSystem(int m, int t)
+    {
+        minimumVotes = m;
+        totalSources = t;
+    }
+
+    //Setters
+    public void setMinimumVotes(int m)
+    {
+        minimumVotes = m;
+    }
+
+    public void setTotalSources(int m)
+    {
+        minimumVotes = m;
+    }
+
+    //Getters
+    public int getMinimumVotes()
+    {
+        return minimumVotes;
+    }
+
+    public int getTotalSources()
+    {
+        return totalSources;
+    }
+
 }
