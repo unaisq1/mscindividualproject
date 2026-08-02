ad1fd8e0633e3bfe8d5d174f84a9b28b7318ef32
Unais Qureshi <unaisq1@gmail.com>
2026-08-03 00:59:19 +0100

    SensorFusionFunction class completed

---
+++ Model Deliverable/JavaImplementation/JavaImplementation/src/com/safetyandsecurityinterplay/functional/SensorFusionFunction.java
@@ -0,0 +1,71 @@
+package com.safetyandsecurityinterplay.functional;
+
+import com.safetyandsecurityinterplay.component.FusionProcessor;
+import com.safetyandsecurityinterplay.component.VotingSystem;
+
+public class SensorFusionFunction {
+
+    //Attributes/Variables
+    private VotingSystem votingSystem;
+    private FusionProcessor fusionProcessor;
+    private int fusionLatencyInMilliseconds;
+    private boolean inputValidationRequired;
+
+    //Constructor
+    public SensorFusionFunction(VotingSystem vs, FusionProcessor fp, int f, boolean i)
+    {
+        votingSystem = vs;
+        fusionProcessor = fp;
+        fusionLatencyInMilliseconds = f;
+        inputValidationRequired = i;
+    }
+
+    //Behaviour
+    //OCL: SFF Constraint 1 (Security to Safety Property): self.inputValidationRequired = true implies self.fusionLatency <= 20
+    public boolean satisfiesSFF1() 
+    {
+        return !inputValidationRequired || fusionLatencyInMilliseconds <= 20;
+    }
+
+    //Setters
+    public void setVotingSystem(VotingSystem vs)
+    {
+        votingSystem = vs;
+    }
+
+    public void setFusionProcessor(FusionProcessor fp)
+    {
+        fusionProcessor = fp;
+    }
+
+    public void setFusionLatencyInMilliseconds(int f)
+    {
+        fusionLatencyInMilliseconds = f;
+    }
+
+    public void setInputValidationRequired(boolean i)
+    {
+        inputValidationRequired = i;
+    }
+
+    //Getters
+    public VotingSystem getVotingSystem() 
+    {
+        return votingSystem;
+    }
+
+    public FusionProcessor getFusionProcessor() 
+    {
+        return fusionProcessor;
+    }
+
+    public int getFusionLatencyInMilliseconds() 
+    {
+        return fusionLatencyInMilliseconds;
+    }
+
+    public boolean getInputValidationRequired() 
+    {
+        return inputValidationRequired;
+    }
+}
